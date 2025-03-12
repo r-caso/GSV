c895bcee3f09b2764873ab630a2beac27cd76043
Ramiro Caso <caso.ramiro@gmail.com>
2025-03-12 08:24:25 -0300

    Add documentation

--- GSV/include/utility/formatter.hpp
+++ GSV/include/utility/formatter.hpp
@@ -4,6 +4,12 @@
 
 namespace iif_sadaf::talk::GSV {
 
+/**
+ * @brief A visitor for formatting Expression objects.
+ *
+ * The `Formatter` struct provides a std::string representation of `Expression`
+ * objects.
+ */
 struct Formatter {
     std::string operator()(std::shared_ptr<UnaryNode> expr) const;
     std::string operator()(std::shared_ptr<BinaryNode> expr) const;
