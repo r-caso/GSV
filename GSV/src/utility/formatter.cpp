a7c585731c1ef21ea82d4cbd17702df5518bcd9f
Ramiro Caso <caso.ramiro@gmail.com>
2025-03-12 08:23:48 -0300

    Add utf8 symbols to Formatter

--- GSV/src/utility/formatter.cpp
+++ GSV/src/utility/formatter.cpp
@@ -4,25 +4,32 @@ namespace iif_sadaf::talk::GSV {
 
 std::string Formatter::operator()(std::shared_ptr<UnaryNode> expr) const
 {
-    std::string op = expr->op == Operator::NEG ? "-" :
-        expr->op == Operator::E_NEC ? "L" :
-        "M";
+    std::string op = expr->op == Operator::NEG ? "¬" :
+        expr->op == Operator::E_NEC ? "□" :
+        "⋄";
+
+    if (typeid(expr->scope) == typeid(std::shared_ptr<IdentityNode>)) {
+        std::string prejacent = std::visit(Formatter(), expr->scope);
+        const std::string::size_type pos = prejacent.find("=");
+        prejacent.replace(pos, 1, "≠");
+        return prejacent;
+    }
 
     return op + std::visit(Formatter(), expr->scope);
 }
 
 std::string Formatter::operator()(std::shared_ptr<BinaryNode> expr) const
 {
-    std::string op = expr->op == Operator::CON ? " & " :
-        expr->op == Operator::DIS ? " v " :
-        " -> ";
+    std::string op = expr->op == Operator::CON ? " ⋀ " :
+        expr->op == Operator::DIS ? " ⋁ " :
+        " → ";
 
     return "(" + std::visit(Formatter(), expr->lhs) + op + std::visit(Formatter(), expr->rhs) + ")";
 }
 
 std::string Formatter::operator()(std::shared_ptr<QuantificationNode> expr) const
 {
-    std::string quantifier = expr->quantifier == Quantifier::EXISTENTIAL ? "E" : "A";
+    std::string quantifier = expr->quantifier == Quantifier::EXISTENTIAL ? "∃" : "∀";
 
     return quantifier + expr->variable.literal + " " + std::visit(Formatter(), expr->scope);
 }
