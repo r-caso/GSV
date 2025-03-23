788c3d301f869e28a3c1384e38b976143d62a417
Ramiro Caso <caso.ramiro@gmail.com>
2025-03-23 13:23:15 -0300

    Add convenience headers for gsv-core and GSV

---
+++ GSV/gsv-core/include/core.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "information_state.hpp"
+#include "possibility.hpp"
+#include "referent_system.hpp"
---
+++ GSV/include/GSV.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "information_state.hpp"
+#include "possibility.hpp"
+#include "referent_system.hpp"
+
+#include "evaluator.hpp"
+#include "semantic_relations.hpp"
+
+#include "imodel.hpp"
