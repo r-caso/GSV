aac5fa0052bc8bf5d37c57e7c739fb4671293684
Ramiro Caso <caso.ramiro@gmail.com>
2025-03-27 21:01:04 -0300

    Use convenience headers

--- GSV/include/GSV.hpp
+++ GSV/include/GSV.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-#include "qml_model_adapter.hpp"
-
-#include "information_state.hpp"
-#include "possibility.hpp"
-#include "referent_system.hpp"
-
+#include "adapters.hpp"
+#include "core.hpp"
 #include "evaluator.hpp"
 #include "semantic_relations.hpp"
 
